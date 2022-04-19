ebeff1d05f5ab3d4cd99e5eb4fdb21615f301c84
yunhozz <qkrdbsgh1121@naver.com>
2022-04-19 20:38:21 +0900

    first commit

---
+++ src/main/java/drinkreview/DrinkReviewApplication.java
@@ -0,0 +1,13 @@
+package drinkreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/drinkreview/DrinkReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package drinkreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
