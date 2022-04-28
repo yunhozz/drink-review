b177977cd4ee79457866b729f2f27accc05271cf
yunhozz <qkrdbsgh1121@naver.com>
2022-04-28 19:41:25 +0900

    first commit

--- src/main/java/drinkreview/DrinkReviewApplication.java
+++ src/main/java/drinkreview/DrinkReviewApplication.java
@@ -2,8 +2,10 @@ package drinkreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DrinkReviewApplication {
 
 	public static void main(String[] args) {
