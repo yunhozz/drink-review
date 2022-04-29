8045155e2ca3b6d35fb29dadc61ee29c90b2329f
yunhozz <qkrdbsgh1121@naver.com>
2022-04-29 11:45:02 +0900

    UserDetails 보완

--- src/main/java/drinkreview/domain/member/Member.java
+++ src/main/java/drinkreview/domain/member/Member.java
@@ -2,9 +2,11 @@ package drinkreview.domain.member;
 
 import drinkreview.domain.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Column;
@@ -12,6 +14,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -20,47 +24,65 @@ public class Member extends BaseEntity implements UserDetails {
 
     @Id
     @GeneratedValue
-    @Column(name = "member_id")
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String memberId;
+
+    @Column(nullable = false)
     private String memberPw;
+
     private String name;
     private int age;
     private String auth;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+    @Builder
+    private Member(String memberId, String memberPw, String name, int age, String auth) {
+        this.memberId = memberId;
+        this.memberPw = memberPw;
+        this.name = name;
+        this.age = age;
+        this.auth = auth;
     }
 
     @Override
-    public String getPassword() {
-        return null;
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> roles = new HashSet<>();
+
+        for (String role : auth.split(",")) {
+            roles.add(new SimpleGrantedAuthority(role));
+        }
+
+        return roles;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return memberId;
+    }
+
+    @Override
+    public String getPassword() {
+        return memberPw;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
