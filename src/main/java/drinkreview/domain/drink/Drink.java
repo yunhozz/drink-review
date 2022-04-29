d73098bca0e31053f6f2f98a94ed6e9328cdc971
yunhozz <qkrdbsgh1121@naver.com>
2022-04-29 15:41:14 +0900

    변수 추가, 비즈니스 로직 추가

--- src/main/java/drinkreview/domain/drink/Drink.java
+++ src/main/java/drinkreview/domain/drink/Drink.java
@@ -1,7 +1,9 @@
 package drinkreview.domain.drink;
 
-import drinkreview.domain.BaseEntity;
+import drinkreview.domain.TimeEntity;
+import drinkreview.global.exception.NotEnoughStockException;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,15 +16,42 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Drink extends BaseEntity {
+public class Drink extends TimeEntity {
 
     @Id
     @GeneratedValue
-    @Column(name = "drink_id")
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
     private String country;
     private LocalDate productionDate;
+    private int price;
+    private double gpa;
     private int stockQuantity;
+
+    @Builder
+    private Drink(String name, String country, LocalDate productionDate, int price, double gpa, int stockQuantity) {
+        this.name = name;
+        this.country = country;
+        this.productionDate = productionDate;
+        this.price = price;
+        this.gpa = gpa;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void addQuantity(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeQuantity(int stockQuantity) {
+        int remainQuantity = this.stockQuantity - stockQuantity;
+
+        if (remainQuantity < 0) {
+            throw new NotEnoughStockException("Not enough stock.");
+        }
+
+        this.stockQuantity = remainQuantity;
+    }
 }
