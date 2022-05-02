0b93e92ad4150853cb8568b349e96eedb2119e61
yunhozz <qkrdbsgh1121@naver.com>
2022-05-02 15:05:47 +0900

    조회 쿼리 메소드 추가

--- src/main/java/drinkreview/domain/member/repository/MemberRepository.java
+++ src/main/java/drinkreview/domain/member/repository/MemberRepository.java
@@ -2,10 +2,23 @@ package drinkreview.domain.member.repository;
 
 import drinkreview.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     Optional<Member> findByMemberId(String memberId);
+
+    @Query("select m.memberId from Member m")
+    List<String> findMemberIdList();
+
+    @Query("select m from Member m where m.name = :name and m.age = :age")
+    List<Member> findWithNameAndAge(@Param("name") String name, @Param("age") int age);
+
+    @Query("select m from Member m where m.name in :names")
+    List<Member> findWithNames(@Param("names") Collection<String> names);
 }
