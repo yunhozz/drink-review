5aedf9af8a8697b92af46c77380e3abcc17b06e7
yunhozz <qkrdbsgh1121@naver.com>
2022-05-04 11:24:58 +0900

    키워드 검색 기능 추가

--- src/main/java/drinkreview/domain/review/repository/ReviewRepository.java
+++ src/main/java/drinkreview/domain/review/repository/ReviewRepository.java
@@ -2,6 +2,13 @@ package drinkreview.domain.review.repository;
 
 import drinkreview.domain.review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
+
+    @Query("select r from Review r where r.title like %:keyword% or r.content like %:keyword%")
+    List<Review> findWithKeyword(@Param("keyword") String keyword); //검색 기능
 }
