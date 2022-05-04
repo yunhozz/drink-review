d83d49ca7927d39ea1beecfa3b568cdb161c907c
yunhozz <qkrdbsgh1121@naver.com>
2022-05-04 19:46:48 +0900

    JPAQueryFactory 스프링 빈 삭제

--- src/main/java/drinkreview/DrinkReviewApplication.java
+++ src/main/java/drinkreview/DrinkReviewApplication.java
@@ -1,12 +1,8 @@
 package drinkreview;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -14,10 +10,5 @@ public class DrinkReviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DrinkReviewApplication.class, args);
-	}
-
-	@Bean
-	JPAQueryFactory jpaQueryFactory(EntityManager em) {
-		return new JPAQueryFactory(em);
 	}
 }
