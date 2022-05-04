ec678235021d8a4d6f88fb63c18448411ab4a6e3
yunhozz <qkrdbsgh1121@naver.com>
2022-05-04 11:22:20 +0900

    builder 삭제

--- src/main/java/drinkreview/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/drinkreview/domain/comment/dto/CommentRequestDto.java
@@ -3,7 +3,6 @@ package drinkreview.domain.comment.dto;
 import drinkreview.domain.comment.Comment;
 import drinkreview.domain.member.Member;
 import drinkreview.domain.review.Review;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,13 +13,6 @@ public class CommentRequestDto {
     private Member member;
     private Review review;
     private String comments;
-
-    @Builder
-    private CommentRequestDto(Member member, Review review, String comments) {
-        this.member = member;
-        this.review = review;
-        this.comments = comments;
-    }
 
     public Comment toEntity() {
         return Comment.createComment(member, review, comments);
