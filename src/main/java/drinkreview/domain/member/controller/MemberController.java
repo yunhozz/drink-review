c13bda809580313205a1c28a3f89f9de02121425
yunhozz <qkrdbsgh1121@naver.com>
2022-05-05 11:46:06 +0900

    경로 수정, 로그인&로그아웃 메소드 보완

--- src/main/java/drinkreview/domain/member/controller/MemberController.java
+++ src/main/java/drinkreview/domain/member/controller/MemberController.java
@@ -1,9 +1,12 @@
 package drinkreview.domain.member.controller;
 
 import drinkreview.domain.member.Member;
-import drinkreview.domain.member.MemberService;
 import drinkreview.domain.member.dto.MemberRequestDto;
+import drinkreview.domain.member.service.MemberService;
+import drinkreview.domain.member.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,7 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -19,11 +22,12 @@ import javax.validation.Valid;
 public class MemberController {
 
     private final MemberService memberService;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @GetMapping("/join")
     public String joinForm(Model model) {
         model.addAttribute("memberDto", new MemberRequestDto());
-        return "join";
+        return "member/join";
     }
 
     @PostMapping("/join")
@@ -40,29 +44,25 @@ public class MemberController {
     @GetMapping("/login")
     public String loginForm(Model model) {
         model.addAttribute("loginForm", new LoginForm());
-        return "login";
+        return "member/login";
     }
 
     @PostMapping("/login")
-    public String login(@Valid LoginForm loginForm, BindingResult result, HttpServletRequest request) {
+    public String login(@Valid LoginForm loginForm, BindingResult result, Model model) {
         if (result.hasErrors()) {
             return "member/login";
         }
 
-        Member member = memberService.login(loginForm.getMemberId(), loginForm.getMemberPw());
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConstants.LOGIN_MEMBER, member);
+        Member loginMember = memberService.login(loginForm);
+        model.addAttribute("loginMember", loginMember);
 
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletRequest request) {
-        HttpSession session = request.getSession(false);
-
-        if (session != null) {
-            session.invalidate();
-        }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler()
+                .logout(request, response, SecurityContextHolder.getContext().getAuthentication());
 
         return "redirect:/";
     }
