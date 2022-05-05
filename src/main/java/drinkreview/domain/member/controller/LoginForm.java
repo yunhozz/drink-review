41783bc2e5224481ea11b73d85e6d130349eeea5
yunhozz <qkrdbsgh1121@naver.com>
2022-05-05 20:04:09 +0900

    어노테이션 수정

--- src/main/java/drinkreview/domain/member/controller/LoginForm.java
+++ src/main/java/drinkreview/domain/member/controller/LoginForm.java
@@ -1,12 +1,10 @@
 package drinkreview.domain.member.controller;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
-@NoArgsConstructor
 public class LoginForm {
 
     @NotEmpty private String memberId;
