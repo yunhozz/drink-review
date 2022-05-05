c13ba6bd872bf9a910c2ac71690d990ad9c6d47a
yunhozz <qkrdbsgh1121@naver.com>
2022-05-05 20:06:01 +0900

    findMember 메소드 변경

--- src/main/java/drinkreview/domain/member/service/MemberService.java
+++ src/main/java/drinkreview/domain/member/service/MemberService.java
@@ -3,6 +3,7 @@ package drinkreview.domain.member.service;
 import drinkreview.domain.member.Member;
 import drinkreview.domain.member.controller.LoginForm;
 import drinkreview.domain.member.dto.MemberRequestDto;
+import drinkreview.domain.member.dto.MemberResponseDto;
 import drinkreview.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -43,14 +44,18 @@ public class MemberService {
 
     //회원 탈퇴
     public void withdraw(Long id) {
-        Member member = this.findMember(id);
+        Member member = memberRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("Member is null."));
+
         memberRepository.delete(member);
     }
 
     @Transactional(readOnly = true)
-    public Member findMember(Long id) {
-        return memberRepository.findById(id)
+    public MemberResponseDto findMember(Long id) {
+        Member member = memberRepository.findById(id)
                 .orElseThrow(() -> new IllegalStateException("Member is null."));
+
+        return new MemberResponseDto(member);
     }
 
     @Transactional(readOnly = true)
