55297692a47ac2fc9bc24ae199cf6bd401860672
yunhozz <qkrdbsgh1121@naver.com>
2022-05-08 19:30:53 +0900

    childList 변수 삭제, 생성자 변경

--- src/main/java/drinkreview/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/drinkreview/domain/comment/dto/CommentRequestDto.java
@@ -6,8 +6,6 @@ import drinkreview.domain.review.Review;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 public class CommentRequestDto {
@@ -16,9 +14,8 @@ public class CommentRequestDto {
     private Review review;
     private String content;
     private Comment parent;
-    private List<Comment> childList;
 
     public Comment toEntity() {
-        return Comment.createComment(member, review, content, parent, childList);
+        return Comment.createComment(member, review, content, parent);
     }
 }
