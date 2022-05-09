0fdce431ab25ba25a0ff6be8a65a962c38044f68
yunhozz <qkrdbsgh1121@naver.com>
2022-05-09 18:10:23 +0900

    OrderRepositoryCustom 확장, 쿼리 메소드 생성

---
+++ src/main/java/drinkreview/domain/order/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package drinkreview.domain.order.repository;
+
+import drinkreview.domain.order.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
+
+    @Query(value = "select o from Order o join fetch o.member m",
+            countQuery = "select count(o) from Order o")
+    Page<Order> findPage(Pageable pageable);
+
+    @Query("select m.name from Order o join fetch o.member m")
+    List<String> findMemberNameWithOrder();
+
+    @Query("select o from Order o where o.status = 'ORDER'")
+    List<Order> findCompleteOrder();
+}
