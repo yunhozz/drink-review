82ec01e60c5f60981f978c86a17963491ca9b070
yunhozz <qkrdbsgh1121@naver.com>
2022-05-09 11:56:02 +0900

    parent, child 관련 쿼리 삭제

--- src/main/java/drinkreview/domain/comment/repository/CommentRepositoryImpl.java
+++ src/main/java/drinkreview/domain/comment/repository/CommentRepositoryImpl.java
@@ -1,9 +1,7 @@
 package drinkreview.domain.comment.repository;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import drinkreview.domain.comment.Comment;
-import drinkreview.domain.comment.dto.ChildResponseDto;
 import drinkreview.domain.comment.dto.CommentQueryDto;
 import drinkreview.domain.comment.dto.CommentResponseDto;
 import drinkreview.domain.comment.dto.QCommentQueryDto;
@@ -27,14 +25,10 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
 
         List<Comment> comments = queryFactory
                 .selectFrom(comment)
-                .join(comment.member).fetchJoin()
-                .join(comment.review).fetchJoin()
-                .leftJoin(comment.parent).fetchJoin()
+                .join(comment.member, member).fetchJoin()
+                .join(comment.review, review).fetchJoin()
                 .where(review.id.eq(reviewId))
-                .orderBy(
-                        comment.parent.id.asc().nullsFirst(),
-                        comment.createdDate.asc()
-                )
+                .orderBy(comment.createdDate.asc())
                 .fetch();
 
         for (Comment comment : comments) {
@@ -47,14 +41,9 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
 
     @Override
     public List<CommentQueryDto> searchList(Long reviewId) {
-        queryFactory
+        return queryFactory
                 .select(new QCommentQueryDto(
                         comment.id,
-                        comment.parent.id,
-                        Projections.constructor(
-                                ChildResponseDto.class,
-
-                        )
                         comment.content,
                         comment.createdDate,
                         member.id,
@@ -64,12 +53,8 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
                 ))
                 .join(comment.member, member)
                 .join(comment.review, review)
-                .join(comment.parent)
                 .where(review.id.eq(reviewId))
-                .orderBy(
-                        comment.parent.id.asc().nullsFirst(),
-                        comment.createdDate.asc()
-                )
+                .orderBy(comment.createdDate.asc())
                 .fetch();
     }
 }
