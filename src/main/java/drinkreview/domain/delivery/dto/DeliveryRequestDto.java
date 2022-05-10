db5c66accdd421671055b48c7635ef2d1b90964a
yunhozz <qkrdbsgh1121@naver.com>
2022-05-10 17:09:40 +0900

    Order 추가

--- src/main/java/drinkreview/domain/delivery/dto/DeliveryRequestDto.java
+++ src/main/java/drinkreview/domain/delivery/dto/DeliveryRequestDto.java
@@ -2,6 +2,7 @@ package drinkreview.domain.delivery.dto;
 
 import drinkreview.domain.Address;
 import drinkreview.domain.delivery.Delivery;
+import drinkreview.domain.order.Order;
 import drinkreview.global.enums.City;
 import drinkreview.global.enums.DeliveryStatus;
 import lombok.Data;
@@ -11,12 +12,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class DeliveryRequestDto {
 
+    private Order order;
     private City city;
     private String street;
     private String etc;
 
     public Delivery toEntity() {
         Address address = new Address(city, street, etc);
-        return new Delivery(address, DeliveryStatus.PREPARING);
+        return new Delivery(order, address, DeliveryStatus.PREPARING);
     }
 }
