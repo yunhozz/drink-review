7493f143a4d9c59ed2303a14790df7e3206b745b
yunhozz <qkrdbsgh1121@naver.com>
2022-05-11 17:58:46 +0900

    쿼리 메소드 추가

--- src/main/java/drinkreview/domain/orderDrink/OrderDrinkRepository.java
+++ src/main/java/drinkreview/domain/orderDrink/OrderDrinkRepository.java
@@ -1,6 +1,13 @@
 package drinkreview.domain.orderDrink;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderDrinkRepository extends JpaRepository<OrderDrink, Long> {
+
+    //전체 상품 각각의 재고보다 주문량이 많은 주문들
+    @Query("select od from OrderDrink od where od.count > all(select d.stockQuantity from Drink d)")
+    List<OrderDrink> findOrderThanDrinkStock();
 }
