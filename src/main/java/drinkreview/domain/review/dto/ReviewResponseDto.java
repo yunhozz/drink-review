b50a935351b32981e76dbad99d7429c13a45ec85
yunhozz <qkrdbsgh1121@naver.com>
2022-05-13 19:13:33 +0900

    memberName 추가

--- src/main/java/drinkreview/domain/review/dto/ReviewResponseDto.java
+++ src/main/java/drinkreview/domain/review/dto/ReviewResponseDto.java
@@ -16,6 +16,7 @@ public class ReviewResponseDto {
     private Long drinkId;
     private String title;
     private String content;
+    private String memberName;
     private double score;
     private int view;
     private List<CommentResponseDto> comments;
@@ -26,6 +27,7 @@ public class ReviewResponseDto {
         this.drinkId = review.getDrink().getId();
         this.title = review.getTitle();
         this.content = review.getContent();
+        this.memberName = review.getMemberName();
         this.score = review.getScore();
         this.view = review.getView();
         this.comments = review.getComments().stream()
