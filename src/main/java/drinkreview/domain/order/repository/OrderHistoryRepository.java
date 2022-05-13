bdbfacd3b184ef962284e1273af9cb8d64e7d2c7
yunhozz <qkrdbsgh1121@naver.com>
2022-05-13 19:10:02 +0900

    findWithUserId 쿼리 추가

--- src/main/java/drinkreview/domain/order/repository/OrderHistoryRepository.java
+++ src/main/java/drinkreview/domain/order/repository/OrderHistoryRepository.java
@@ -2,10 +2,13 @@ package drinkreview.domain.order.repository;
 
 import drinkreview.domain.order.OrderHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
 
-    Optional<OrderHistory> findByUserId(Long userId);
+    @Query("select oh from OrderHistory oh where oh.memberInfo.userId = :id")
+    Optional<OrderHistory> findWithUserId(@Param("id") Long userId);
 }
