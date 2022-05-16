7a1b1fb9935ac8ffe8ad65a278ee8454b3f3a8db
yunhozz <qkrdbsgh1121@naver.com>
2022-05-16 12:23:29 +0900

    어노테이션 변경

--- src/main/java/drinkreview/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/drinkreview/domain/comment/dto/CommentRequestDto.java
@@ -3,11 +3,11 @@ package drinkreview.domain.comment.dto;
 import drinkreview.domain.comment.Comment;
 import drinkreview.domain.member.Member;
 import drinkreview.domain.review.Review;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
 public class CommentRequestDto {
 
     private Member member;
