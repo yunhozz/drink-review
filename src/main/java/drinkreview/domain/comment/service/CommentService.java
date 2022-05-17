f313b33a026d718ee4818d330643c34c9ab651f7
yunhozz <qkrdbsgh1121@naver.com>
2022-05-17 12:36:32 +0900

    deleteComment 대댓글 존재 확인 로직 추가

--- src/main/java/drinkreview/domain/comment/service/CommentService.java
+++ src/main/java/drinkreview/domain/comment/service/CommentService.java
@@ -1,5 +1,6 @@
-package drinkreview.domain.comment;
+package drinkreview.domain.comment.service;
 
+import drinkreview.domain.comment.Comment;
 import drinkreview.domain.comment.dto.CommentRequestDto;
 import drinkreview.domain.comment.dto.CommentResponseDto;
 import drinkreview.domain.comment.repository.CommentRepository;
@@ -23,7 +24,6 @@ public class CommentService {
     public Long makeComment(CommentRequestDto dto, Long userId, Long reviewId) {
         Member member = memberRepository.findById(userId)
                 .orElseThrow(() -> new IllegalStateException("Member is null."));
-
         Review review = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new IllegalStateException("Review is null."));
 
@@ -34,10 +34,11 @@ public class CommentService {
         return comment.getId();
     }
 
-    public void updateComment(CommentRequestDto dto, Long commentId, Long userId) {
+    public void updateComment(CommentRequestDto dto, Long commentId) {
         Comment comment = this.findComment(commentId);
 
-        if (!comment.getMember().getId().equals(userId)) {
+        //작성자만 수정 가능
+        if (!comment.getMember().getId().equals(dto.getMember().getId())) {
             throw new IllegalStateException("You do not have permission.");
         }
 
@@ -51,8 +52,12 @@ public class CommentService {
 
         //댓글 작성자만 삭제 가능
         if (comment.getMember().getId().equals(member.getId())) {
-            commentRepository.delete(comment);
-
+            //대댓글이 없으면 아예 삭제, 하나라도 있으면 내용만 변경
+            if (comment.getCommentChildList().isEmpty()) {
+                commentRepository.delete(comment);
+            } else {
+                comment.delete();
+            }
         } else {
             throw new IllegalStateException("You do not have permission.");
         }
