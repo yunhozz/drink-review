de4bf90140c7ba74e0f56c9548f68f2a00168b85
yunhozz <qkrdbsgh1121@naver.com>
2022-05-17 16:20:19 +0900

    deleteCommentChild 메소드 보완

--- src/main/java/drinkreview/domain/comment/service/CommentChildService.java
+++ src/main/java/drinkreview/domain/comment/service/CommentChildService.java
@@ -7,6 +7,7 @@ import drinkreview.domain.comment.repository.CommentChildRepository;
 import drinkreview.domain.comment.repository.CommentRepository;
 import drinkreview.domain.member.Member;
 import drinkreview.domain.member.repository.MemberRepository;
+import drinkreview.global.enums.DeleteStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,14 +45,23 @@ public class CommentChildService {
         commentChild.updateContent(dto.getContent());
     }
 
-    public void deleteCommentChild(Long commentChildId, Long userId) {
+    public void deleteCommentChild(Long commentChildId, Long commentId, Long userId) {
         CommentChild commentChild = this.findCommentChild(commentChildId);
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new IllegalStateException("Comment is null."));
         Member member = memberRepository.findById(userId)
                 .orElseThrow(() -> new IllegalStateException("Member is null."));
 
         //댓글 작성자만 삭제 가능
         if (commentChild.getMember().getId().equals(member.getId())) {
-            commentChild.delete();
+            //댓글 삭제 상태 -> 대댓글 하나 ?
+            if (comment.getIsDeleted() == DeleteStatus.Y) {
+                if (commentChildRepository.isLastOne()) {
+                    commentRepository.delete(comment); //cascade delete : CommentChild
+                } else {
+                    commentChildRepository.delete(commentChild);
+                }
+            }
         } else {
             throw new IllegalStateException("You do not have permission.");
         }
