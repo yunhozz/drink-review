e545da1ab8e250e3d3bbd61bf8ad058e3128b78a
yunhozz <qkrdbsgh1121@naver.com>
2022-05-20 18:09:57 +0900

    세션 관련 보완

--- src/main/java/drinkreview/global/controller/HomeController.java
+++ src/main/java/drinkreview/global/controller/HomeController.java
@@ -1,13 +1,29 @@
 package drinkreview.global.controller;
 
+import drinkreview.domain.member.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute("loginMember");
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
