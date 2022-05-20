8885ded7884603cb40a1bb59606dbc300b1604b3
yunhozz <qkrdbsgh1121@naver.com>
2022-05-20 18:12:43 +0900

    @NotBlank 삭제

--- src/main/java/drinkreview/domain/orderDrink/dto/OrderDrinkForm.java
+++ src/main/java/drinkreview/domain/orderDrink/dto/OrderDrinkForm.java
@@ -2,11 +2,9 @@ package drinkreview.domain.orderDrink.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 public class OrderDrinkForm {
 
-    @NotBlank private Long drinkId;
+    private Long drinkId;
     private int count;
 }
