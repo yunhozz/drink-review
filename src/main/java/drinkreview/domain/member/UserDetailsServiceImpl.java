d295518f12667a025ff7ad09ca9ee7106474da18
yunhozz <qkrdbsgh1121@naver.com>
2022-05-20 18:07:00 +0900

    first commit

--- src/main/java/drinkreview/domain/member/UserDetailsServiceImpl.java
+++ src/main/java/drinkreview/domain/member/UserDetailsServiceImpl.java
@@ -1,8 +1,5 @@
-package drinkreview.domain.member.service;
+package drinkreview.domain.member;
 
-import drinkreview.domain.member.Member;
-import drinkreview.domain.member.UserDetailsImpl;
-import drinkreview.domain.member.dto.MemberSessionResponseDto;
 import drinkreview.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,23 +7,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 @Component
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final MemberRepository memberRepository;
-    private final HttpServletRequest request;
 
-    //스프링 시큐리티 세션에 유저 정보 저장
+    /**
+     * @param memberId
+     * @return UserDetailsImpl
+     * @throws UsernameNotFoundException
+     */
     @Override
     public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
         Member member = memberRepository.findByMemberId(memberId)
                 .orElseThrow(() -> new UsernameNotFoundException(memberId));
-        HttpSession session = request.getSession();
-        session.setAttribute("member", new MemberSessionResponseDto(member));
 
         return new UserDetailsImpl(member);
     }
