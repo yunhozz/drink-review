0845ef724d300801d641f5094186c01bd946f593
yunhozz <qkrdbsgh1121@naver.com>
2022-05-23 16:50:10 +0900

    home 메소드 보완

--- src/main/java/drinkreview/global/controller/HomeController.java
+++ src/main/java/drinkreview/global/controller/HomeController.java
@@ -1,17 +1,21 @@
 package drinkreview.global.controller;
 
+import drinkreview.domain.member.controller.LoginForm;
 import drinkreview.domain.member.controller.SessionConstant;
+import drinkreview.domain.member.dto.MemberRequestDto;
 import drinkreview.domain.member.dto.MemberSessionResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(@SessionAttribute(name = SessionConstant.LOGIN_MEMBER, required = false) MemberSessionResponseDto loginMember, Model model) {
+    public String home(@ModelAttribute MemberRequestDto memberRequestDto, @ModelAttribute LoginForm loginForm,
+                       @SessionAttribute(name = SessionConstant.LOGIN_MEMBER, required = false) MemberSessionResponseDto loginMember, Model model) {
         if (loginMember == null) {
             return "home";
         }
