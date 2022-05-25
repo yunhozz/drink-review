73ec40263a3e06d485bfdc0c1a5fdc13ca90851a
yunhozz <qkrdbsgh1121@naver.com>
2022-05-25 16:53:14 +0900

    페이징 메소드 추가

--- src/main/java/drinkreview/domain/drink/repository/DrinkRepositoryCustom.java
+++ src/main/java/drinkreview/domain/drink/repository/DrinkRepositoryCustom.java
@@ -1,10 +1,14 @@
 package drinkreview.domain.drink.repository;
 
 import drinkreview.domain.drink.dto.DrinkSimpleResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface DrinkRepositoryCustom {
 
     List<DrinkSimpleResponseDto> searchSimpleDrink();
+    Page<DrinkSimpleResponseDto> searchSimplePageDrink(Pageable pageable);
+    Page<DrinkSimpleResponseDto> searchSimplePageDrinkByKeyword(String keyword, Pageable pageable);
 }
--- src/main/java/drinkreview/domain/drink/repository/DrinkRepositoryImpl.java
+++ src/main/java/drinkreview/domain/drink/repository/DrinkRepositoryImpl.java
@@ -4,6 +4,9 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import drinkreview.domain.drink.dto.DrinkSimpleResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -28,5 +31,46 @@ public class DrinkRepositoryImpl implements DrinkRepositoryCustom {
                 .from(drink)
                 .orderBy(drink.createdDate.desc())
                 .fetch();
+    }
+
+    @Override
+    public Page<DrinkSimpleResponseDto> searchSimplePageDrink(Pageable pageable) {
+        List<DrinkSimpleResponseDto> content = queryFactory
+                .select(Projections.constructor(
+                        DrinkSimpleResponseDto.class,
+                        drink.id,
+                        drink.name,
+                        drink.price,
+                        drink.image,
+                        drink.gpa
+                ))
+                .from(drink)
+                .orderBy(drink.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(content, pageable, content.size());
+    }
+
+    @Override
+    public Page<DrinkSimpleResponseDto> searchSimplePageDrinkByKeyword(String keyword, Pageable pageable) {
+        List<DrinkSimpleResponseDto> content = queryFactory
+                .select(Projections.constructor(
+                        DrinkSimpleResponseDto.class,
+                        drink.id,
+                        drink.name,
+                        drink.price,
+                        drink.image,
+                        drink.gpa
+                ))
+                .from(drink)
+                .where(drink.name.contains(keyword))
+                .orderBy(drink.createdDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(content, pageable, content.size());
     }
 }
