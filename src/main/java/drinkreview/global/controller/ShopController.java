15d3c276ab0340088a25168658f9be6b30190cc3
yunhozz <qkrdbsgh1121@naver.com>
2022-05-25 16:58:07 +0900

    페이징 추가

--- src/main/java/drinkreview/global/controller/ShopController.java
+++ src/main/java/drinkreview/global/controller/ShopController.java
@@ -3,11 +3,12 @@ package drinkreview.global.controller;
 import drinkreview.domain.drink.dto.DrinkSimpleResponseDto;
 import drinkreview.domain.drink.repository.DrinkRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,8 +17,8 @@ public class ShopController {
     private final DrinkRepository drinkRepository;
 
     @GetMapping("/shop")
-    public String drinkList2(Model model) {
-        List<DrinkSimpleResponseDto> drinks = drinkRepository.searchSimpleDrink();
+    public String drinkList(@PageableDefault(size = 8) Pageable pageable, Model model) {
+        Page<DrinkSimpleResponseDto> drinks = drinkRepository.searchSimplePageDrink(pageable);
         model.addAttribute("drinks", drinks);
 
         return "shop";
