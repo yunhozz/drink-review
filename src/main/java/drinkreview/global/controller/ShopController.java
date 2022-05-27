82ca3e7b86628d4d6c8db56860abe262dff64a8a
yunhozz <qkrdbsgh1121@naver.com>
2022-05-27 19:21:20 +0900

    음료 리스트 반환 로직 추가

--- src/main/java/drinkreview/global/controller/ShopController.java
+++ src/main/java/drinkreview/global/controller/ShopController.java
@@ -9,18 +9,30 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/shop")
 @RequiredArgsConstructor
 public class ShopController {
 
     private final DrinkRepository drinkRepository;
 
-    @GetMapping("/shop")
-    public String drinkList(@PageableDefault(size = 8) Pageable pageable, Model model) {
+    @GetMapping
+    public String drinkView(@PageableDefault(size = 8) Pageable pageable, Model model) {
         Page<DrinkSimpleResponseDto> drinks = drinkRepository.searchSimplePageDrink(pageable);
         model.addAttribute("drinks", drinks);
 
         return "shop";
+    }
+
+    @GetMapping("/drink/list")
+    public String drinkList(Model model) {
+        List<DrinkSimpleResponseDto> drinks = drinkRepository.searchSimpleDrink();
+        model.addAttribute("drinks", drinks);
+
+        return "drink/list";
     }
 }
