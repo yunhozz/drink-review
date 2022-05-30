e1addae18053876c8031db8bc483ff5051d68602
yunhozz <qkrdbsgh1121@naver.com>
2022-05-30 14:15:58 +0900

    viewDrink 보완

--- src/main/java/drinkreview/domain/drink/controller/DrinkController.java
+++ src/main/java/drinkreview/domain/drink/controller/DrinkController.java
@@ -1,13 +1,15 @@
 package drinkreview.domain.drink.controller;
 
-import drinkreview.domain.drink.dto.DrinkResponseDto;
 import drinkreview.domain.drink.DrinkService;
+import drinkreview.domain.drink.dto.DrinkResponseDto;
+import drinkreview.domain.order.controller.OrderForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.PostConstruct;
 
@@ -18,28 +20,28 @@ public class DrinkController {
 
     private final DrinkService drinkService;
 
-    @GetMapping("/drink")
-    public String drinkView(@RequestParam("id") Long drinkId, Model model) {
+    @GetMapping("/drink/{id}")
+    public String viewDrink(@PathVariable("id") Long drinkId, @ModelAttribute OrderForm orderForm, Model model) {
         DrinkResponseDto drink = drinkService.findDrinkDto(drinkId);
         model.addAttribute("drink", drink);
 
-        return "drink/detail";
+        return "drink/drink-detail";
     }
 
     @PostConstruct
     public void init() throws Exception {
-        for (int i = 1; i <= 8; i++) {
+        for (int i = 1; i <= 30; i++) {
             DrinkForm form = new DrinkForm();
             form.setName("cola" + i);
             form.setCountry("Korea");
-            form.setYear(2022);
-            form.setMonth(i);
-            form.setDay(i * 2);
+            form.setYear(1996);
+            form.setMonth(1);
+            form.setDay(11);
             form.setPrice(i * 1000);
             form.setStockQuantity(i * 10);
 
             drinkService.saveDrink(form);
-            Thread.sleep(10);
+            Thread.sleep(5);
         }
     }
 }
