696490ee7be303dbbdf1653b8a9ac86216cb66d8
yunhozz <qkrdbsgh1121@naver.com>
2022-05-30 14:16:28 +0900

    벌크 업데이트 쿼리 추가

--- src/main/java/drinkreview/domain/drink/repository/DrinkRepository.java
+++ src/main/java/drinkreview/domain/drink/repository/DrinkRepository.java
@@ -4,12 +4,26 @@ import drinkreview.domain.drink.Drink;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface DrinkRepository extends JpaRepository<Drink, Long>, DrinkRepositoryCustom {
+
+    //판매량 업데이트
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Drink d set d.salesVolume = d.salesVolume + 1 where d.id = :drinkId")
+    void updateSalesVol(@Param("drinkId") Long drinkId);
+
+    //평점 업데이트
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Drink d set d.gpa = ((d.gpa * salesVolume) + :score) / (salesVolume + 1) where d.id = :drinkId")
+    void updateGpa(@Param("drinkId") Long drinkId, @Param("score") double score);
 
     //특정 나라의 음료 리스트 조회
     @Query("select d from Drink d where d.country = :country")
