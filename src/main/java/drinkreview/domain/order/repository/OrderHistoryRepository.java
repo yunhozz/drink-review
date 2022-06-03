f4684d4c667fda3d2aa7097aae9f2629fdd73ee8
yunhozz <qkrdbsgh1121@naver.com>
2022-06-03 17:27:03 +0900

    쿼리 보완

--- src/main/java/drinkreview/domain/order/repository/OrderHistoryRepository.java
+++ src/main/java/drinkreview/domain/order/repository/OrderHistoryRepository.java
@@ -1,6 +1,6 @@
 package drinkreview.domain.order.repository;
 
-import drinkreview.domain.order.OrderHistory;
+import drinkreview.domain.order.history.OrderHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
 
-    @Query("select oh from OrderHistory oh where oh.memberInfo.userId = :id")
-    Optional<OrderHistory> findWithUserId(@Param("id") Long userId);
+    @Query("select oh from OrderHistory oh where oh.memberInfo.userId = :userId")
+    Optional<OrderHistory> findWithUserId(@Param("userId") Long userId);
 }
