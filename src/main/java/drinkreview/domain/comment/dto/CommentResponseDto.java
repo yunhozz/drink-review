6c3cd119fb0e99eb7cf0df59a5fe9bc3faf6167b
yunhozz <qkrdbsgh1121@naver.com>
2022-06-03 17:23:17 +0900

    변수 추가

--- src/main/java/drinkreview/domain/comment/dto/CommentResponseDto.java
+++ src/main/java/drinkreview/domain/comment/dto/CommentResponseDto.java
@@ -2,24 +2,32 @@ package drinkreview.domain.comment.dto;
 
 import drinkreview.domain.comment.Comment;
 import drinkreview.global.enums.DeleteStatus;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class CommentResponseDto {
 
     private Long id;
     private Long userId;
     private Long reviewId;
     private String content;
+    private String memberName;
     private DeleteStatus isDeleted;
+    private List<CommentChildResponseDto> commentChildList;
 
     public CommentResponseDto(Comment comment) {
         this.id = comment.getId();
         this.userId = comment.getMember().getId();
         this.reviewId = comment.getReview().getId();
         this.content = comment.getContent();
+        this.memberName = comment.getMemberName();
         this.isDeleted = comment.getIsDeleted();
+        this.commentChildList = comment.getCommentChildList().stream()
+                .map(CommentChildResponseDto::new)
+                .toList();
     }
 }
