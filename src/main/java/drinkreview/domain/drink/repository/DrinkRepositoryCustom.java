07f0e4e439fe78e44f52299be884d62b6b052443
yunhozz <qkrdbsgh1121@naver.com>
2022-06-03 17:26:15 +0900

    searchSimpleDrink 삭제

--- src/main/java/drinkreview/domain/drink/repository/DrinkRepositoryCustom.java
+++ src/main/java/drinkreview/domain/drink/repository/DrinkRepositoryCustom.java
@@ -4,11 +4,8 @@ import drinkreview.domain.drink.dto.DrinkSimpleResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface DrinkRepositoryCustom {
 
-    List<DrinkSimpleResponseDto> searchSimpleDrink();
     Page<DrinkSimpleResponseDto> searchSimplePageDrink(Pageable pageable);
     Page<DrinkSimpleResponseDto> searchSimplePageDrinkByKeyword(String keyword, Pageable pageable);
 }
