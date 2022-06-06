e4734008d1c2f8b3ff05bea11c5201296806eb3e
yunhozz <qkrdbsgh1121@naver.com>
2022-06-06 19:39:00 +0900

    판매량 업데이트 -> 리뷰 갯수 업데이트

--- src/main/java/drinkreview/domain/drink/repository/DrinkRepository.java
+++ src/main/java/drinkreview/domain/drink/repository/DrinkRepository.java
@@ -11,24 +11,29 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DrinkRepository extends JpaRepository<Drink, Long>, DrinkRepositoryCustom {
-
-    //판매량 업데이트
-    @Transactional
-    @Modifying(clearAutomatically = true)
-    @Query("update Drink d set d.salesVolume = d.salesVolume + 1 where d.id = :drinkId")
-    void updateSalesVol(@Param("drinkId") Long drinkId);
 
     //평점 업데이트
     @Transactional
     @Modifying(clearAutomatically = true)
-    @Query("update Drink d set d.gpa = ((d.gpa * salesVolume) + :score) / (salesVolume + 1) where d.id = :drinkId")
+    @Query("update Drink d set d.gpa = ((d.gpa * d.evaluationCount) + :score) / (d.evaluationCount + 1) where d.id = :drinkId")
     void updateGpa(@Param("drinkId") Long drinkId, @Param("score") double score);
+
+    //리뷰 갯수 업데이트
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Drink d set d.evaluationCount = d.evaluationCount + 1 where d.id = :drinkId")
+    void updateEvaluationCount(@Param("drinkId") Long drinkId);
 
     //특정 유저의 주문 리스트에 있는 음료 이름 조회
     @Query("select distinct d.name from Order o join o.orderDrinks od join od.drink d where o.member.id = :userId")
     List<String> findDrinkNamesInOrder(@Param("userId") Long userId);
+
+    //음료수 이름으로 ID 조회
+    @Query("select d.id from Drink d where d.name = :name")
+    Optional<Long> findDrinkIdWithName(@Param("name") String name);
 
     //특정 나라의 음료 리스트 조회
     @Query("select d from Drink d where d.country = :country")
