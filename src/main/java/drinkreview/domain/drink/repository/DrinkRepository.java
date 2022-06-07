4a59648054d191be1c586b688412992d65e3d140
yunhozz <qkrdbsgh1121@naver.com>
2022-06-07 16:04:29 +0900

    @Transactional 삭제

--- src/main/java/drinkreview/domain/drink/repository/DrinkRepository.java
+++ src/main/java/drinkreview/domain/drink/repository/DrinkRepository.java
@@ -1,14 +1,12 @@
 package drinkreview.domain.drink.repository;
 
 import drinkreview.domain.drink.Drink;
-import drinkreview.domain.orderDrink.OrderDrink;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,13 +14,11 @@ import java.util.Optional;
 public interface DrinkRepository extends JpaRepository<Drink, Long>, DrinkRepositoryCustom {
 
     //평점 업데이트
-    @Transactional
     @Modifying(clearAutomatically = true)
     @Query("update Drink d set d.gpa = ((d.gpa * d.evaluationCount) + :score) / (d.evaluationCount + 1) where d.id = :drinkId")
     void updateGpa(@Param("drinkId") Long drinkId, @Param("score") double score);
 
     //리뷰 갯수 업데이트
-    @Transactional
     @Modifying(clearAutomatically = true)
     @Query("update Drink d set d.evaluationCount = d.evaluationCount + 1 where d.id = :drinkId")
     void updateEvaluationCount(@Param("drinkId") Long drinkId);
@@ -35,9 +31,9 @@ public interface DrinkRepository extends JpaRepository<Drink, Long>, DrinkReposi
     @Query("select d.id from Drink d where d.name = :name")
     Optional<Long> findDrinkIdWithName(@Param("name") String name);
 
-    //특정 나라의 음료 리스트 조회
-    @Query("select d from Drink d where d.country = :country")
-    List<Drink> findDrinksOfCountry(@Param("country") String country);
+    //리뷰 ID로 음료수 이름 조회
+    @Query("select d.name from Review r join r.drink d where r.id = :reviewId")
+    Optional<String> findDrinkNameWithReviewId(@Param("reviewId") Long reviewId);
 
     //평균 이상의 gpa 를 가진 음료 리스트 조회
     @Query("select d from Drink d where d.gpa >= (select avg(d1.gpa) from Drink d1)")
