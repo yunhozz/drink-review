29e0278ee24090e49fca4716f8a0d6670e968f8a
yunhozz <qkrdbsgh1121@naver.com>
2022-06-07 16:06:56 +0900

    read, update 메소드 보완

--- src/main/java/drinkreview/domain/review/controller/ReviewController.java
+++ src/main/java/drinkreview/domain/review/controller/ReviewController.java
@@ -5,7 +5,6 @@ import drinkreview.domain.member.dto.MemberSessionResponseDto;
 import drinkreview.domain.review.ReviewService;
 import drinkreview.domain.review.dto.ReviewRequestDto;
 import drinkreview.domain.review.dto.ReviewResponseDto;
-import drinkreview.domain.review.repository.ReviewRepository;
 import drinkreview.global.controller.LoginSessionConstant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -19,26 +18,42 @@ import javax.validation.Valid;
 import java.util.List;
 
 @Controller
-@RequestMapping("/community")
+@RequestMapping("/community/review")
 @RequiredArgsConstructor
 public class ReviewController {
 
     private final ReviewService reviewService;
-    private final ReviewRepository reviewRepository;
     private final DrinkRepository drinkRepository;
 
-    @GetMapping("/review/{id}")
-    public String read(@PathVariable("id") Long reviewId, Model model) {
+    @GetMapping("/{id}")
+    public String read(@PathVariable("id") Long reviewId, @SessionAttribute(LoginSessionConstant.LOGIN_MEMBER) MemberSessionResponseDto loginMember, Model model) {
+        if (loginMember == null) {
+            return "member/login";
+        }
+
         ReviewResponseDto review = reviewService.findReviewDto(reviewId);
-        reviewRepository.addView(review.getId());
+        reviewService.addView(review.getId());
         model.addAttribute("review", review);
+
+        String drinkName = drinkRepository.findDrinkNameWithReviewId(reviewId)
+                .orElseThrow(() -> new IllegalStateException("Can't find drink's name."));
+        model.addAttribute("drinkName", drinkName);
+
+        //review, comment 작성자만 update 버튼 표시
+        if (review.getUserId().equals(loginMember.getId())) {
+            model.addAttribute("reviewWriter", true);
+        }
+        for (int i = 0; i < review.getComments().size(); i++) {
+            boolean isCommentWriter = review.getComments().get(i).getUserId().equals(loginMember.getId());
+            model.addAttribute("commentWriter", isCommentWriter);
+        }
 
         return "review/review-detail";
     }
 
-    @GetMapping("/review/write")
-    public String write(@SessionAttribute(LoginSessionConstant.LOGIN_MEMBER) MemberSessionResponseDto loginMember,
-                        @ModelAttribute ReviewRequestDto reviewRequestDto, Model model) {
+    @GetMapping("/write")
+    public String write(@SessionAttribute(LoginSessionConstant.LOGIN_MEMBER) MemberSessionResponseDto loginMember, @ModelAttribute ReviewRequestDto reviewRequestDto,
+                        Model model) {
         if (loginMember == null) {
             return "member/login";
         }
@@ -50,7 +65,7 @@ public class ReviewController {
         return "review/write";
     }
 
-    @PostMapping("/review/write")
+    @PostMapping("/write")
     public String write(@Valid ReviewRequestDto reviewRequestDto, BindingResult result, HttpServletRequest request) {
         if (result.hasErrors()) {
             return "review/write";
@@ -62,33 +77,40 @@ public class ReviewController {
         String drinkName = request.getParameter("drinkName");
         Long drinkId = drinkRepository.findDrinkIdWithName(drinkName)
                 .orElseThrow(() -> new IllegalStateException("Can't find drink with this name : " + drinkName));
-        reviewService.makeReview(reviewRequestDto, loginMember.getId(), drinkId);
 
+        reviewService.makeReview(reviewRequestDto, loginMember.getId(), drinkId);
         return "redirect:/community";
     }
 
-    @GetMapping("/review/{id}/update")
+    @GetMapping("/{id}/update")
     public String update(@SessionAttribute(LoginSessionConstant.LOGIN_MEMBER) MemberSessionResponseDto loginMember, @PathVariable("id") Long reviewId,
                          @ModelAttribute ReviewRequestDto reviewRequestDto, Model model) {
         if (loginMember == null) {
             return "member/login";
         }
 
-        return "review/write";
+        ReviewResponseDto review = reviewService.findReviewDto(reviewId);
+        model.addAttribute("review", review);
+
+        return "review/update";
     }
 
-    @PostMapping("/review/{id}/update")
-    public String update(@PathVariable("id") Long reviewId, @Valid ReviewRequestDto reviewRequestDto, BindingResult result,
-                         @RequestParam("userId") Long userId) {
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable("id") Long reviewId, @Valid ReviewRequestDto reviewRequestDto, BindingResult result, HttpServletRequest request) {
         if (result.hasErrors()) {
-            return "review/write";
+            return "review/update";
         }
 
-        reviewService.updateReview(reviewRequestDto, reviewId, userId);
+        HttpSession session = request.getSession();
+        MemberSessionResponseDto loginMember = (MemberSessionResponseDto) session.getAttribute(LoginSessionConstant.LOGIN_MEMBER);
+
+        ReviewResponseDto review = reviewService.findReviewDto(reviewId);
+        reviewService.updateReview(reviewRequestDto, review.getId(), loginMember.getId());
+
         return "redirect:/community";
     }
 
-    @GetMapping("/review/{id}/delete")
+    @GetMapping("/{id}/delete")
     public String delete(@SessionAttribute(LoginSessionConstant.LOGIN_MEMBER) MemberSessionResponseDto loginMember, @PathVariable("id") Long reviewId) {
         if (loginMember == null) {
             return "member/login";
