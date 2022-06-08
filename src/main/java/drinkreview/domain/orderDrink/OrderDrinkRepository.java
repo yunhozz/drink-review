39cb0e15f54a44f3d44293f7df990ae53b4e2a1c
yunhozz <qkrdbsgh1121@naver.com>
2022-06-08 17:59:18 +0900

    findWithUserId 추가

--- src/main/java/drinkreview/domain/orderDrink/OrderDrinkRepository.java
+++ src/main/java/drinkreview/domain/orderDrink/OrderDrinkRepository.java
@@ -1,6 +1,14 @@
 package drinkreview.domain.orderDrink;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderDrinkRepository extends JpaRepository<OrderDrink, Long> {
+
+    //userId 로 조회
+    @Query("select od from OrderDrink od join fetch od.order o join fetch o.member m where m.id = :userId")
+    List<OrderDrink> findWithUserId(@Param("userId") Long userId);
 }
