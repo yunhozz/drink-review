191606e1d60310f80b440120dc3ee0df7518842c
yunhozz <qkrdbsgh1121@naver.com>
2022-06-09 17:15:04 +0900

    생성자 수정, update 추가

--- src/main/java/drinkreview/domain/orderDrink/Cart.java
+++ src/main/java/drinkreview/domain/orderDrink/Cart.java
@@ -1,21 +1,27 @@
 package drinkreview.domain.orderDrink;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cart {
 
     private Long drinkId;
     private int count;
     private int orderPrice;
 
-    public Cart(OrderDrinkResponseDto orderDrink) {
-        drinkId = orderDrink.getDrinkId();
-        count = orderDrink.getCount();
-        orderPrice = orderDrink.getOrderPrice();
+    public Cart(Long drinkId, int count, int orderPrice) {
+        this.drinkId = drinkId;
+        this.count = count;
+        this.orderPrice = orderPrice;
+    }
+
+    public void update(int count, int orderPrice) {
+        this.count = count;
+        this.orderPrice = orderPrice;
     }
 
     @Getter
