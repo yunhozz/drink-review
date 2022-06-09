8a30ddc4f4a795b170c36e37b4865dcdc986f8fb
yunhozz <qkrdbsgh1121@naver.com>
2022-06-09 17:19:16 +0900

    shop, cart 추가

--- src/main/java/drinkreview/global/controller/ShopController.java
+++ src/main/java/drinkreview/global/controller/ShopController.java
@@ -1,7 +1,9 @@
 package drinkreview.global.controller;
 
-import drinkreview.domain.drink.dto.DrinkSimpleResponseDto;
+import drinkreview.domain.drink.dto.DrinkQueryDto;
 import drinkreview.domain.drink.repository.DrinkRepository;
+import drinkreview.domain.orderDrink.Cart;
+import drinkreview.domain.orderDrink.CartList;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,8 +11,11 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,18 +26,39 @@ public class ShopController {
     private final DrinkRepository drinkRepository;
 
     @GetMapping
-    public String drinkView(@PageableDefault(size = 8) Pageable pageable, Model model) {
-        Page<DrinkSimpleResponseDto> drinks = drinkRepository.searchSimplePageDrink(pageable);
+    public String shop(@SessionAttribute(SessionConstant.CART_LIST) CartList cartList, @PageableDefault(size = 8) Pageable pageable, Model model) {
+        Page<DrinkQueryDto> drinks = drinkRepository.searchSimplePageDrink(pageable);
         model.addAttribute("drinks", drinks);
+        model.addAttribute("carts", cartList.getCarts());
 
         return "shop";
     }
 
-    @GetMapping("/drink/list")
-    public String drinkList(Model model) {
-        List<DrinkSimpleResponseDto> drinks = drinkRepository.searchSimpleDrink();
-        model.addAttribute("drinks", drinks);
+    @GetMapping("/cart")
+    public String cart(@SessionAttribute(SessionConstant.CART_LIST) CartList cartList, Model model) {
+        List<Cart> carts = cartList.getCarts();
+        List<DrinkQueryDto> drinks = new ArrayList<>();
+        int totalPrice = 0;
 
-        return "drink/list";
+        for (Cart cart : carts) {
+            DrinkQueryDto drink = drinkRepository.searchDrinkOnCart(cart.getDrinkId());
+            drinks.add(drink);
+            totalPrice += cart.getOrderPrice();
+        }
+        model.addAttribute("drinks", drinks);
+        model.addAttribute("carts", carts);
+        model.addAttribute("totalPrice", totalPrice);
+
+        return "cart";
+    }
+
+    @GetMapping("/cart/cancel/{id}")
+    public String cartCancel(@SessionAttribute(SessionConstant.CART_LIST) CartList cartList, @PathVariable("id") Long drinkId) {
+        return null;
+    }
+
+    @GetMapping("/cart/order")
+    public String cartOrder() {
+        return null;
     }
 }
