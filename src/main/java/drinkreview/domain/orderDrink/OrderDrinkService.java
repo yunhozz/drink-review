12361e36db4e8fc8d6dcdb203f0f6a9f77223cfa
yunhozz <qkrdbsgh1121@naver.com>
2022-06-11 18:48:05 +0900

    findOrderDrinkDtoList 추가

--- src/main/java/drinkreview/domain/orderDrink/OrderDrinkService.java
+++ src/main/java/drinkreview/domain/orderDrink/OrderDrinkService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -23,8 +26,26 @@ public class OrderDrinkService {
     }
 
     @Transactional(readOnly = true)
+    public List<OrderDrinkResponseDto> findOrderDrinkDtoList() {
+        List<OrderDrinkResponseDto> orderDrinkDtoList = new ArrayList<>();
+        List<OrderDrink> orderDrinkList = this.findOrderDrinkList();
+
+        for (OrderDrink orderDrink : orderDrinkList) {
+            OrderDrinkResponseDto orderDrinkDto = new OrderDrinkResponseDto(orderDrink);
+            orderDrinkDtoList.add(orderDrinkDto);
+        }
+
+        return orderDrinkDtoList;
+    }
+
+    @Transactional(readOnly = true)
     private OrderDrink findOrderDrink(Long orderDrinkId) {
         return orderDrinkRepository.findById(orderDrinkId)
                 .orElseThrow(() -> new IllegalStateException("OrderDrink is null."));
+    }
+
+    @Transactional(readOnly = true)
+    private List<OrderDrink> findOrderDrinkList() {
+        return orderDrinkRepository.findAll();
     }
 }
