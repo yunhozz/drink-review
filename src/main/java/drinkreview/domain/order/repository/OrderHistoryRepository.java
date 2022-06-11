0ed4a2d7a71872d4ce9f0fe00abcd9f1b6c4facb
yunhozz <qkrdbsgh1121@naver.com>
2022-06-11 18:49:12 +0900

    orderHistory 파일 이동에 따른 수정

--- src/main/java/drinkreview/domain/order/repository/OrderHistoryRepository.java
+++ src/main/java/drinkreview/domain/order/repository/OrderHistoryRepository.java
@@ -1,6 +1,6 @@
 package drinkreview.domain.order.repository;
 
-import drinkreview.domain.order.history.OrderHistory;
+import drinkreview.domain.order.OrderHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
