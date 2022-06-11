642490db8201dfa62aaed4d7591b78c8271ee153
yunhozz <qkrdbsgh1121@naver.com>
2022-06-11 18:48:36 +0900

    orderEntity 파일 이동에 따른 수정

--- src/main/java/drinkreview/domain/order/repository/OrderEntityRepository.java
+++ src/main/java/drinkreview/domain/order/repository/OrderEntityRepository.java
@@ -1,6 +1,6 @@
 package drinkreview.domain.order.repository;
 
-import drinkreview.domain.order.history.OrderEntity;
+import drinkreview.domain.order.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderEntityRepository extends JpaRepository<OrderEntity, Long> {
