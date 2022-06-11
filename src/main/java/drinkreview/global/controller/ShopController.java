b748d171bf3909549759836ae645242a17eb28f0
yunhozz <qkrdbsgh1121@naver.com>
2022-06-11 18:51:57 +0900

    orderList 추가

--- src/main/java/drinkreview/global/controller/ShopController.java
+++ src/main/java/drinkreview/global/controller/ShopController.java
@@ -2,8 +2,14 @@ package drinkreview.global.controller;
 
 import drinkreview.domain.drink.dto.DrinkQueryDto;
 import drinkreview.domain.drink.repository.DrinkRepository;
+import drinkreview.domain.member.dto.MemberSessionResponseDto;
+import drinkreview.domain.order.OrderService;
+import drinkreview.domain.order.dto.OrderHistoryResponseDto;
+import drinkreview.domain.order.dto.OrderResponseDto;
 import drinkreview.domain.orderDrink.Cart;
 import drinkreview.domain.orderDrink.CartList;
+import drinkreview.domain.orderDrink.OrderDrinkResponseDto;
+import drinkreview.domain.orderDrink.OrderDrinkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,14 +21,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/shop")
 @RequiredArgsConstructor
 public class ShopController {
 
+    private final OrderService orderService;
+    private final OrderDrinkService orderDrinkService;
     private final DrinkRepository drinkRepository;
 
     @GetMapping
@@ -52,13 +64,52 @@ public class ShopController {
         return "cart";
     }
 
+    @GetMapping("/order-list")
+    public String orderList(@SessionAttribute(SessionConstant.LOGIN_MEMBER) MemberSessionResponseDto loginMember, Model model) {
+        if (loginMember == null) {
+            return "member/login";
+        }
+
+        List<OrderResponseDto> orders = new ArrayList<>();
+        OrderHistoryResponseDto orderHistory = orderService.findOrderHistoryDto(loginMember.getId());
+
+        for (int i = 0; i < orderHistory.getOrderEntities().size(); i++) {
+            Long orderId = orderHistory.getOrderEntities().get(i).getOrderId();
+            OrderResponseDto order = orderService.findOrderDto(orderId);
+            orders.add(order);
+        }
+        model.addAttribute("orderHistory", orderHistory);
+        model.addAttribute("orders", orders);
+
+        List<OrderDrinkResponseDto> orderDrinkList = orderDrinkService.findOrderDrinkDtoList();
+        model.addAttribute("orderDrinkList", orderDrinkList);
+
+        return "order/order-list";
+    }
+
     @GetMapping("/cart/cancel/{id}")
     public String cartCancel(@SessionAttribute(SessionConstant.CART_LIST) CartList cartList, @PathVariable("id") Long drinkId) {
-        return null;
+        Cart cart = cartList.getCarts().stream().filter(c -> c.getDrinkId().equals(drinkId)).findFirst()
+                        .orElseThrow(() -> new IllegalStateException("Cart is null."));
+        cartList.getCarts().remove(cart);
+
+        return "redirect:/shop";
     }
 
     @GetMapping("/cart/order")
-    public String cartOrder() {
-        return null;
+    public String cartOrder(@SessionAttribute(SessionConstant.CART_LIST) CartList cartList, HttpServletRequest request) {
+        Map<Long, Integer> orderMap = new HashMap<>();
+        List<Cart> carts = cartList.getCarts();
+
+        for (Cart cart : carts) {
+            orderMap.put(cart.getDrinkId(), cart.getCount());
+        }
+        orderService.makeOrderByMap(cartList.getUserId(), orderMap);
+
+        HttpSession session = request.getSession(false);
+        session.removeAttribute(SessionConstant.CART_LIST);
+        session.setAttribute(SessionConstant.CART_LIST, new CartList(cartList.getUserId()));
+
+        return "redirect:/shop";
     }
 }
