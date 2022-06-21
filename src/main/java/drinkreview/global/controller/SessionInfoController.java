bcb1a75214975ffcba90cee903328cda92b50a63
yunhozz <qkrdbsgh1121@naver.com>
2022-06-21 12:13:26 +0900

    cookie 부분 추가

--- src/main/java/drinkreview/global/controller/SessionInfoController.java
+++ src/main/java/drinkreview/global/controller/SessionInfoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Date;
@@ -26,6 +27,17 @@ public class SessionInfoController {
         log.info("creationTime={}", new Date(session.getCreationTime()));
         log.info("lastAccessedTime={}", new Date(session.getLastAccessedTime()));
         log.info("isNew={}", session.isNew());
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie == null) {
+                return "쿠키가 존재하지 않습니다";
+            } else {
+                log.info("cookieName={}", cookie.getName());
+                log.info("cookieValue={}", cookie.getValue());
+                log.info("cookieMaxAge={}", cookie.getMaxAge());
+            }
+        }
 
         return "세션 출력";
     }
