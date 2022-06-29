1c8f7baac17764e63d6f53e494e8af351cdf6883
yunhozz <qkrdbsgh1121@naver.com>
2022-06-29 16:20:13 +0900

    어노테이션 변경

--- src/main/java/drinkreview/domain/member/controller/LoginForm.java
+++ src/main/java/drinkreview/domain/member/controller/LoginForm.java
@@ -1,10 +1,12 @@
 package drinkreview.domain.member.controller;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
-@Data
+@Getter
+@Setter
 public class LoginForm {
 
     @NotBlank(message = "You must enter an ID.")
